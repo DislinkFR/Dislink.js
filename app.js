c53f42ed8bb90fb2f8b6a96fc937ff4c2fb65dd1
SkudarTV <kidpumu@gmail.com>
2021-07-03 15:49:10 +0200

    Update to v1.2
    Add "info" function,
    Update "links" function for compatibility with the API

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
-const axios = require("axios").default;
+const axios = require("axios").default,
+    configuration = require("./configuration.json"),
+    FilterError = require("./error/filterError.js");
+
 
 class Main {
+
     constructor() {
-        this.version = "1.0"
-    }
-    links = async() => {
-        var list = await axios.get("https://api.dislink.fr/links/");
-        return list.data;
+        const _version = configuration.version;
+        const _branch = configuration.branch;
+
+        this.info = () => {
+            console.log("#### Dislink.JS ####");
+            console.log(`Version : ${_version}`);
+            console.log(`Branche : ${_branch}`);
+            console.log("#### ########## ####");
+        }
+        this.links = async(option = {}) => {
+            const filterOption = ["click", "name"];
+            console.log(filterOption.includes(option.filter))
+            if (option.filter && !filterOption.includes(option.filter)) {
+                console.log("Unappropried filter.\n Try one of this list :\n" + filterOption.join(", "))
+                return;
+            }
+            switch (option.filter) {
+                case "click":
+                    var list = await axios.get("https://api.dislink.fr/links/click");
+                    break;
+                case "name":
+                    var list = await axios.get("https://api.dislink.fr/links/name");
+                    break;
+                default:
+                    var list = await axios.get("https://api.dislink.fr/links/");
+                    break;
+            }
+
+            return list.data;
+        }
     }
 }
 
---
+++ error/filterError.js
@@ -0,0 +1,10 @@
+class FilterError extends Error {
+    constructor(message) {
+        super(message)
+        this.name = this.constructor.name
+        Error.captureStackTrace(this, this.constructor);
+        this.isSleepy = true
+    }
+}
+
+module.exports = FilterError
