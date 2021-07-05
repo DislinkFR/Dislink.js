599a4e622383b61a1afce2fa1932800cfb2cd84c
SkudarTV <kidpumu@gmail.com>
2021-07-05 17:22:07 +0200

    update to 1.4.0

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const axios = require("axios").default,
-    configuration = require("./configuration.json"),
-    FilterError = require("./error/filterError.js");
-
+    configuration = require("./configuration.json")
 
 class Main {
-
     constructor() {
+        console.log(this.links)
         const _version = configuration.version;
         const _branch = configuration.branch;
 
@@ -15,28 +13,9 @@ class Main {
             console.log(`Branche : ${_branch}`);
             console.log("#### ########## ####");
         }
-        this.links = async(option = {}) => {
-            const filterOption = ["clicks", "name"];
-            console.log(filterOption.includes(option.filter))
-            if (option.filter && !filterOption.includes(option.filter)) {
-                console.log("Unappropried filter.\n Try one of this list :\n" + filterOption.join(", "))
-                return;
-            }
-            switch (option.filter) {
-                case "clicks":
-                    var list = await axios.get("https://api.dislink.fr/links/clicks");
-                    break;
-                case "name":
-                    var list = await axios.get("https://api.dislink.fr/links/name");
-                    break;
-                default:
-                    var list = await axios.get("https://api.dislink.fr/links/");
-                    break;
-            }
-
-            return list.data;
-        }
     }
 }
+
+Main.links = require("./lib/links.js")
 
 module.exports = Main
